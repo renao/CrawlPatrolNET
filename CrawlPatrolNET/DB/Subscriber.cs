a48e4d10493bec94a936baf93ddc2c248b9c3d1a
René Wernicke <r.wernicke@prosoz.de>
2020-07-27 22:11:42 +0200

    Add Subscriber DB Model

---
+++ CrawlPatrolNET/DB/Subscriber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CrawlPatrolNET.DB
+{
+    public class Subscriber
+    {
+        public Guid Id { get; set; }
+        public long ChatId { get; set; }
+
+        public Subscriber()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
